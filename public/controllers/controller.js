8b71ecff1adb41a9dba42571c28707915278ffcc
TxsAdamWest <Twinadanz@gmail.com>
2016-10-25 20:10:45 -0500

    set up 3

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,14 @@
+// myApp is the entire application that will be hosted on the server.
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http',function($scope,$http){
 	console.log('Waddup from controller!');
+
+// This is essentially writing an API.  Here, I will define what actions are 
+// taken once certain commands are requested.  
+$http.get('/contactList').success(function(response){
+	console.log("I got the data I requested!");
+	$scope.contactList = response;
+});
+
+	
 }])
--- server.js
+++ server.js
@@ -1,9 +1,37 @@
+//First, we must import our express as a dependency once it is installed.
 var express = require('express');
 var app = express();
 
+//This will serve as a directory to watch for files.
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(req, res){
+	console.log("GET request recieved!")
 
+	person1 = {
+		name: 'Jago',
+		email: 'tigerspirit@ki.com',
+		number: '555-555-5555'
+	}
+
+	person2 = {
+		name: 'Sabrewulf',
+		email: 'wolfman@ki.com',
+		number: '555-555-6666'
+	}
+
+	person3 = {
+		name: 'Orchid',
+		email: 'bbt@ki.com',
+		number: '555-555-7777'
+	}
+
+	var contactList = [person1, person2, person3];
+
+	res.json(contactList)
+})
+
+//We select the port the app will run on.
 app.listen(3000);
 console.log("Server up and running on port 3000");
 
